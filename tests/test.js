2cbfeab3a8de85e64998ea72dda5addecd1cdcee
enjoyablee <65438150+enjoyablee@users.noreply.github.com>
2021-09-18 12:27:22 -0500

    This is not the commit message you are looking for

---
+++ tests/test.js
@@ -0,0 +1,14 @@
+const credential = require("./credential.json");
+
+// quick.fb
+const Database = require("../index");
+const db = new Database("", credential);
+
+const player = "enjoyablee";
+
+db.set(`players/${player}/items`, ["Helmet"]).then(() => {
+  // => ["Helmet"]
+  db.push(`players/${player}/items`, "Sword").then((result) => {
+    console.log(result); // => ["Helmet", "Sword"]
+  });
+});
